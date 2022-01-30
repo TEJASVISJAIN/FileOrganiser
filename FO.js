11aedbbd5c5c0fb1ba5a18cb0bdfd03498bd1821
Tejasvi S Jain <72309538+TEJASVISJAIN@users.noreply.github.com>
2022-01-30 16:49:46 +0530

    Add files via upload

---
+++ FO.js
@@ -0,0 +1,24 @@
+const helpModule = require("./commands/help");
+const organizeModule = require("./commands/organize");
+const treeModule = require("./commands/tree");
+let inputArr = process.argv.slice(2);
+
+//[Node FO.js tree folderpath]
+
+let command = inputArr[0];
+
+switch (command) {
+  case "tree":
+    console.log("Tree Implemented")
+    break;
+  case "organize":
+    organizeModule.organizeKey(inputArr[1]);
+    break;
+  case "help":
+    helpModule.helpKey();
+    break;
+
+  default:
+    console.log("PLEASE ENTER A VALID Command");
+    break;
+}
